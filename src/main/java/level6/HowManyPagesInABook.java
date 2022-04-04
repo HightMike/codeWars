47be54f23db935f6dc8e0a3c6a55444c7d982660
mrborisov <mrborisov@rtech.ru>
2022-04-05 00:29:52 +0300

    add solution for HowManyPagesInABook

--- src/main/java/level6/HowManyPagesInABook.java
+++ src/main/java/level6/HowManyPagesInABook.java
@@ -3,7 +3,22 @@ package level6;
 public class HowManyPagesInABook {
 
     public static int amountOfPages(int summary) {
-        return 0;
+        double target = 0;
+        int i = 0;
+        int k = 1;
+        double preResult = 0;
+        if (summary < 10) {
+            return summary;
+        }
+        while (target < summary) {
+            double v = 9 * Math.pow(10, i);
+            target = target + v * k;
+            preResult = preResult + v;
+            i++;
+            k++;
+        }
+        double diff = (target - summary) / (k-1);
+        return (int) (preResult - diff);
     }
 
 }
